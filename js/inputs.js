ffdefb45cad4f5b404c2720e7c74a41c5ae37954
oddzye <tzariov.dmitry@yandex.ru>
2019-07-08 16:16:52 +0300

    mvc

--- js/inputs.js
+++ js/inputs.js
@@ -1,20 +1,21 @@
 (function () {
     const input = document.getElementById('add-item-input');
     const button = document.querySelector('.add-btn');
-    const ul = document.querySelector('.todo-list'); 
-    let inputValue = '';
+    const ul = document.querySelector('.todo-list');
+    let task = {
+        name: '',
+        id: null
+    };
     input.addEventListener('change', ({target: {value}}) => {
-        inputValue = value;
-        console.log(inputValue);
+        task.name = value;
     })
     button.addEventListener('click', (e) => {
         e.preventDefault();
         const li = document.createElement('li');
-        const liText = document.createTextNode(`${inputValue}`);
+        const liText = document.createTextNode(`${task.name}`);
         li.appendChild(liText);
         ul.appendChild(li);
-        inputValue = '';
+        task.name = '';
         input.value = '';
     })
-
 }());
